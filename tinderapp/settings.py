8b6e3a8de6364bbac42755b1780654358c53c7c6
unknown <User@DESKTOP-KE3H6VP.union.local>
2026-04-12 21:36:35 +0200

    connect remote mysql db

--- tinderapp/settings.py
+++ tinderapp/settings.py
@@ -61,10 +61,10 @@ ASGI_APPLICATION = 'tinderapp.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tinderapp_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'NAME': 'pdnwqioi_mbinga',
+        'USER': 'pdnwqioi_prince',
+        'PASSWORD': 'FAST_2020',
+        'HOST': '57.128.188.247',
         'PORT': '3306',
         'OPTIONS': {
             'charset': 'utf8mb4',
